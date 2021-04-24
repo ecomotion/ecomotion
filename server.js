dc92f935898dd4696da79408f8e49c7906171f1f
allanmaclean <allanmaclean@github.com>
2021-04-24 11:07:31 -0400

    passport set up but not working

--- server.js
+++ server.js
@@ -39,4 +39,60 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
+
+
+
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./passport');
+
+
+
+//Configure Session Storage
+app.use(cookieSession({
+  name: 'session-name',
+  keys: ['key1', 'key2']
+}))
+
+//Configure Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Unprotected Routes
+// app.get('/', (req, res) => {
+//   res.send('<h1>Home</h1>')
+// });
+
+app.get('/failed', (req, res) => {
+  res.send('<h1>Log in Failed :(</h1>')
+});
+
+// Middleware - Check user is Logged in
+const checkUserLoggedIn = (req, res, next) => {
+  req.user ? next() : res.sendStatus(401);
+}
+
+//Protected Route.
+app.get('/profile', checkUserLoggedIn, (req, res) => {
+  res.send(`<h1>${req.user.displayName}'s Profile Page</h1>`)
+});
+
+// Auth Routes
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function (req, res) {
+    res.redirect('/profile');
+  }
+);
+
+//Logout
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+})
+
+
+
 app.listen(PORT);
